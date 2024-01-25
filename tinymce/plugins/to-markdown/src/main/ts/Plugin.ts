a2d1ec4424a8aa4f031033503a7bd90b7e92b8eb
JaiNarayanan <47904405+JaiNarayanan@users.noreply.github.com>
2024-01-25 00:13:25 -0500

    feat: test dialog plugin

--- tinymce/plugins/to-markdown/src/main/ts/Plugin.ts
+++ tinymce/plugins/to-markdown/src/main/ts/Plugin.ts
@@ -2,11 +2,45 @@ import { Editor, TinyMCE } from 'tinymce';
 
 declare const tinymce: TinyMCE;
 
+var openDialog = function (editor) {
+  return editor.windowManager.open({
+    title: 'Example plugin',
+    body: {
+      type: 'panel',
+      items: [
+        {
+          type: 'input',
+          name: 'title',
+          label: 'Title'
+        }
+      ]
+    },
+    buttons: [
+      {
+        type: 'cancel',
+        text: 'Close'
+      },
+      {
+        type: 'submit',
+        text: 'Save',
+        primary: true
+      }
+    ],
+    onSubmit: function (api) {
+      var data = api.getData();
+      /* Insert content when the window form is submitted */
+      editor.insertContent('Title: ' + data.title);
+      api.close();
+    }
+  });
+};
+
 const setup = (editor: Editor, url: string): void => {
-  editor.ui.registry.addButton('to-markdown', {
-    text: 'to-markdown button',
-    onAction: () => {
-      editor.setContent('<p>content added from to-markdown</p>');
+  editor.ui.registry.addButton('example', {
+    text: 'My button',
+    onAction: function () {
+      /* Open window */
+      openDialog(editor);
     }
   });
 };
